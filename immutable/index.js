f9f610b3ce55a794355a16d2d01d02945fa684c9
Zalmoxisus <zalmoxisus@gmail.com>
2018-12-02 20:21:46 +0200

    Handle references

--- immutable/index.js
+++ immutable/index.js
@@ -1,10 +1,24 @@
 var jsan = require('jsan');
 var serialize = require('./serialize');
 
+var stringifyOptions = {
+  'refs': false,
+  'date': true,
+  'function': true,
+  'regex': true,
+  'undefined': true,
+  'error': true,
+  'symbol': true,
+  'map': true,
+  'set': true,
+  'nan': true,
+  'infinity': true
+};
+
 module.exports = function(Immutable, refs) {
   return {
     stringify: function(data) {
-      return jsan.stringify(data, serialize(Immutable, refs).replacer, null, true);
+      return jsan.stringify(data, serialize(Immutable, refs).replacer, null, stringifyOptions);
     },
     parse: function(data) {
       return jsan.parse(data, serialize(Immutable, refs).reviver);

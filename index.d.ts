f28c5b769f13d67493e2049d7b25a19f6aef8cf2
Julian Hundeloh <jaulz@users.noreply.github.com>
2019-03-07 09:36:10 +0100

    fix: export serialize

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,26 @@
 import Immutable from 'immutable'
 
+type Options = Record<string, boolean>
+
+type Refs = Record<string, any>
+
+type DefaultReplacer = (key:string, value:any) => any
+
+type Replacer = (key:string, value:any, replacer:DefaultReplacer) => any
+
+type DefaultReviver = (key:string, value:any) => any
+
+type Reviver = (key:string, value:any, reviver:DefaultReviver) => any
+
 export namespace RemotedevSerialize {
-  function immutable(immutable:typeof Immutable): {
+  function immutable(immutable:typeof Immutable, refs?:Refs, customReplacer?:Replacer, customReviver?:Reviver): {
     stringify: (input:any) => string,
-    parse: (input:string) => any
+    parse: (input:string) => any,
+    serialize: (immutable:typeof Immutable, refs?:Refs, customReplacer?:Replacer, customReviver?:Reviver) => {
+      replacer: Replacer,
+      reviver: Reviver,
+      options: Options
+    }  
   }
 }
 
